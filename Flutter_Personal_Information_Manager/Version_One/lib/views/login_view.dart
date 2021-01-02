2c4c6d7cd08bbb4e8d04fa9d44c82fdb97f8e020
Ilamah, Osho <oilamah@gmail.com>
2021-01-02 16:59:58 +0300

    Replaced deprecated widgets

--- Flutter_Personal_Information_Manager/Version_One/lib/views/login_view.dart
+++ Flutter_Personal_Information_Manager/Version_One/lib/views/login_view.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'secured/home_view.dart';
+import '../services/services.dart';
 
 final String logo = "assets/images/logo.png";
+bool accountIsSetup = false;
 
 class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    _checkAccount();
     return Scaffold(
       //appBar: null,
       backgroundColor: Colors.white,
@@ -15,14 +18,14 @@ class LoginView extends StatelessWidget {
           padding: EdgeInsets.all(20.0),
           child: Column(children: <Widget>[
             SizedBox(
-              height: 30.0,
+              height: 100.0,
             ),
             Image.asset(
               logo,
               height: 100,
             ),
             SizedBox(
-              height: 30.0,
+              height: 60.0,
             ),
             //
             TextField(
@@ -40,31 +43,44 @@ class LoginView extends StatelessWidget {
             ),
             SizedBox(
               width: double.infinity,
-              child: RaisedButton(
-                padding: EdgeInsets.symmetric(vertical: 10.0),
-                color: Theme.of(context).primaryColor,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(padding: EdgeInsets.symmetric(vertical: 10.0)),
+                //color: Theme.of(context).primaryColor,
                 onPressed: () {
-                  Navigator.pushReplacement(context, MaterialPageRoute(
-                    builder: (BuildContext context)=>HomeView()
-                  ));
+                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => HomeView()));
                 },
                 child: Text("Login", style: TextStyle(color: Colors.white, fontSize: 20.0)),
               ),
             ),
-            MaterialButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => SignupView()));
-              },
-              child: Text("Not a member yet? Create Account", style: TextStyle(color: Colors.blue)),
-            )
+            SizedBox(
+              width: double.infinity,
+              child: Visibility(
+                child: OutlinedButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => SignupView()));
+                  },
+                  child: Text("Not set up yet? Create Account", style: TextStyle(color: Colors.blue)),
+                ),
+                visible: !accountIsSetup,
+              ),
+            ),
           ]),
         ),
       ),
     );
   }
+
+  _checkAccount() async {
+    accountIsSetup = await hasAccount();
+  }
 }
 
 class SignupView extends StatelessWidget {
+  final _fullnameTextController = TextEditingController();
+  final _emailTextController = TextEditingController();
+  final _password2TextController = TextEditingController();
+  final _passwordTextController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -81,12 +97,14 @@ class SignupView extends StatelessWidget {
                 padding: EdgeInsets.all(20.0),
                 child: Column(children: <Widget>[
                   TextField(
+                    controller: _fullnameTextController,
                     decoration: InputDecoration(border: OutlineInputBorder(), hintText: "Full Name", prefixIcon: Icon(Icons.face), fillColor: Colors.white54, filled: true),
                   ),
                   SizedBox(
                     height: 10.0,
                   ),
                   TextField(
+                    controller: _emailTextController,
                     decoration: InputDecoration(border: OutlineInputBorder(), hintText: "Email address", prefixIcon: Icon(Icons.alternate_email), fillColor: Colors.white54, filled: true),
                   ),
                   SizedBox(
@@ -94,6 +112,7 @@ class SignupView extends StatelessWidget {
                   ),
                   TextField(
                     obscureText: true,
+                    controller: _passwordTextController,
                     decoration: InputDecoration(border: OutlineInputBorder(), hintText: "Password", prefixIcon: Icon(Icons.vpn_key), fillColor: Colors.white54, filled: true),
                   ),
                   SizedBox(
@@ -101,6 +120,7 @@ class SignupView extends StatelessWidget {
                   ),
                   TextField(
                     obscureText: true,
+                    controller: _password2TextController,
                     decoration: InputDecoration(border: OutlineInputBorder(), hintText: "Confirm Password", prefixIcon: Icon(Icons.vpn_key), fillColor: Colors.white54, filled: true),
                   ),
                   SizedBox(
@@ -108,20 +128,41 @@ class SignupView extends StatelessWidget {
                   ),
                   SizedBox(
                     width: double.infinity,
-                    child: RaisedButton(
-                      padding: EdgeInsets.symmetric(vertical: 10.0),
-                      color: Theme.of(context).primaryColor,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(padding: EdgeInsets.symmetric(vertical: 10.0)),
+                      //color: Theme.of(context).primaryColor,
                       onPressed: () {
-                        Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => HomeView()));
+                        // String errorMessage = null;
+                        // if (_passwordTextController.text != _password2TextController.text) {
+                        //   errorMessage = 'Passwords do not match!';
+                        // }
+                        // if (_fullnameTextController.text == null || _fullnameTextController.text.length == 0) {
+                        //   errorMessage = 'Fullname must be provided!';
+                        // }
+
+                        // if (_passwordTextController.text == _password2TextController.text) {
+                        //   createAccount(_fullnameTextController.text, _emailTextController.text, _passwordTextController.text).then((success) {
+                        //     if (success) {
+                              Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => HomeView()));
+                        //     }
+                        //   });
+                        // } else {
+                        //   print('Passwords do not match!');
+                        // }
+
+                        //
                       },
                       child: Text("Signup", style: TextStyle(color: Colors.white, fontSize: 20.0)),
                     ),
                   ),
-                  MaterialButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text("Already a member? Login", style: TextStyle(color: Colors.blue)),
+                  SizedBox(
+                    width: double.infinity,
+                    child: OutlinedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text("Already a member? Login", style: TextStyle(color: Colors.blue)),
+                    ),
                   )
                 ]))));
   }
--- Flutter_Personal_Information_Manager/Version_One/lib/views/secured/home_view.dart
+++ Flutter_Personal_Information_Manager/Version_One/lib/views/secured/home_view.dart
@@ -6,27 +6,102 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Lesson 1"),
+          title: Text("Home"),
         ),
-        body: Container(child: SingleChildScrollView(padding: EdgeInsets.all(10.0), child: Column(crossAxisAlignment: CrossAxisAlignment.start, 
-        children: <Widget>[Text("Widgets used", style: Theme.of(context).textTheme.headline4,),
-              ListTile(title: Text("Container")),
-              ListTile(title: Text("BoxDecoration")),
-              ListTile(title: Text("SingleChildScrollView")),
-              ListTile(title: Text("Column")),
-              ListTile(title: Text("TextField")),
-              ListTile(title: Text("InputDecoration")),
-              ListTile(title: Text("AppBar")),
-              ListTile(title: Text("Image")),
-              ListTile(title: Text("ListTile")),
-              MaterialButton(
-                onPressed: (){
-                  Navigator.pushReplacement(context, MaterialPageRoute(
-                    builder: (BuildContext context)=>LoginView()
-                  ));
+        body: Container(
+          padding: EdgeInsets.all(10.0),
+          child: ListView(
+            padding: EdgeInsets.all(10.0),
+            children: <Widget>[
+              TextButton(
+                //style: TextButton.styleFrom(backgroundColor: Colors.red),
+                child: Text("Text Button"),
+                onPressed: () {
+                  //print("flat button pressed");
                 },
-                child: Text("Log Out"),
+              ),
+              ElevatedButton(
+                child: Text("Elevated Button"),
+                //color: Colors.green,
+                onPressed: () {
+                  //print("Elevated button pressed");
+                },
+              ),
+              OutlinedButton(
+                child: Text("Outlined Button"),
+                //color: Colors.deepOrange,
+                onPressed: () {
+                  print("material button presse");
+                },
+              ),
+              ElevatedButton.icon(
+                icon: Icon(Icons.add_a_photo),
+                onPressed: () {
+                  //print("Elevated button with icon pressed");
+                },
+                label: Text('Elevated Button Icon'),
+              ),
+              TextButton.icon(
+                label: Text("Text button icon"),
+                icon: Icon(Icons.play_circle_filled),
+                onPressed: () {
+                  //print("Flat button with icon pressed");
+                },
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Colors.pink, shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0))),
+                child: Text("Rounded Elevated Button"),
+                onPressed: () {
+                  //print("Rounded raised button pressed");
+                },
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Colors.blue, shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(10.0)),),
+                child: Text("Beveled Elevated Button"),
+                onPressed: () {
+                  //print("Beveled raised button pressed");
+                },
+              ),
+              IconButton(
+                color: Colors.deepPurple,
+                iconSize: 40.0,
+                icon: Icon(Icons.plus_one),
+                onPressed: () {
+                  print("Icon button pressed");
+                },
+              ),
+              FloatingActionButton(
+                child: Icon(Icons.dashboard),
+                backgroundColor: Colors.green,
+                onPressed: () {
+                  //print("floating action button 2 pressed");
+                  Navigator.pushReplacement(context, MaterialPageRoute(
+                       builder: (BuildContext context)=>LoginView()
+                     ));
+                },
               )
-              ]))));
+            ],
+          ),
+          // Column(crossAxisAlignment: CrossAxisAlignment.start,
+          // children: <Widget>[Text("Widgets used", style: Theme.of(context).textTheme.headline4,),
+          //       ListTile(title: Text("Container")),
+          //       ListTile(title: Text("BoxDecoration")),
+          //       ListTile(title: Text("SingleChildScrollView")),
+          //       ListTile(title: Text("Column")),
+          //       ListTile(title: Text("TextField")),
+          //       ListTile(title: Text("InputDecoration")),
+          //       ListTile(title: Text("AppBar")),
+          //       ListTile(title: Text("Image")),
+          //       ListTile(title: Text("ListTile")),
+          //       MaterialButton(
+          //         onPressed: (){
+          //           Navigator.pushReplacement(context, MaterialPageRoute(
+          //             builder: (BuildContext context)=>LoginView()
+          //           ));
+          //         },
+          //         child: Text("Log Out"),
+          //       )
+          //       ])
+        ));
   }
 }
