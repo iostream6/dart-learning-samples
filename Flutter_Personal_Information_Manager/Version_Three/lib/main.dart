9cabb61466083cddfac7105727c7b61929fd65d8
Ilamah, Osho <oilamah@gmail.com>
2021-01-08 22:47:51 +0300

    Added database init and provider state management for Notes.

--- Flutter_Personal_Information_Manager/Version_Three/lib/main.dart
+++ Flutter_Personal_Information_Manager/Version_Three/lib/main.dart
@@ -1,19 +1,39 @@
 /*
  * 2021.01.02  - Created
- * 
+ * 2021.01.08  - Added database init and provider state management for Notes
  */
 
 import 'package:flutter/material.dart';
 import 'views/login_view.dart';
 import 'services/services.dart' as services;
+import 'services/sqldb_service.dart' as dao;
+import 'package:provider/provider.dart';
 
 // void main() => runApp(FlutterPIMApp());
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await services.init();
-  runApp(FlutterPIMApp());
+  await dao.getDatabase();
+
+  // Note n =
+  //     Note(null, 'Title', 'Body text', DateTime.now(), DateTime.now(), Colors.blue, false);
+  // dao.insert(n, dao.NOTES_TABLE_NAME);
+  // n =
+  //     Note(null, 'Title2', 'Body text2', DateTime.now(), DateTime.now(), Colors.blue, false);
+  // dao.insert(n, dao.NOTES_TABLE_NAME);
+  // List<Map<String, dynamic>> res = await dao.select(dao.NOTES_TABLE_NAME);
+  // res.forEach((t) async {
+  //   print('Map Result \t ${t.toString()}');
+  //   Note resNote = Note.fromBLOB(t);
+  //   print('OBJ Result:: \t ${resNote.asString()}');
+  //   await dao.delete(resNote, dao.NOTES_TABLE_NAME);
+  // });
+
+  dao.NotesChangeManager ncm = dao.NotesChangeManager();
+  ncm.init();
+  runApp(ChangeNotifierProvider(create: (_) => ncm, child: FlutterPIMApp()));
   //services.init().then((value) => runApp(FlutterPIMApp()));
 }
 
